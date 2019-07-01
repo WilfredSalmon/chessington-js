630810ef462a54c47975fa87dd084434e3c48681
Wilfred Salmon <Wilfred.Salmon@softwire.com>
2019-07-01 11:43:03 +0100

    small change to knight program

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,24 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const allowedMoves = [];
+
+        for (let rowLength = 1; rowLength<3;rowLength++) {
+
+            const colLength = 3 - rowLength;
+
+            for (let rowDirection=-1;rowDirection<2;rowDirection+=2) {
+                for (let colDirection=-1;colDirection<2;colDirection+=2) {
+
+                    const squareToAdd = Square.at(currentSquare.row + rowLength*rowDirection,currentSquare.col + colLength*colDirection)
+                    if(board.checkIfValidSquare(squareToAdd.row,squareToAdd.col)) {
+                        allowedMoves.push(squareToAdd);
+                    }
+                }
+            }
+        }
+
+        return allowedMoves;
     }
 }
