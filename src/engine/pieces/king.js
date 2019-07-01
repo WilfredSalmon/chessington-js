e2811004ac0b63c216ef5a28133eff11bd8dab6b
Wilfred Salmon <Wilfred.Salmon@softwire.com>
2019-07-01 11:49:11 +0100

    Added King moves

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +7,19 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const allowedMoves = [];
+
+        for (let rowAdd = -1; rowAdd<2;rowAdd++) {
+            for (let colAdd = -1; colAdd<2;colAdd++) {
+
+                const squareToAdd = Square.at(currentSquare.row + rowAdd, currentSquare.col + colAdd);
+                if (board.checkIfValidSquare(squareToAdd.row, squareToAdd.col) && !currentSquare.equals(squareToAdd)) {
+                    allowedMoves.push(squareToAdd);
+                }
+            }
+        }
+
+        return allowedMoves;
     }
 }
